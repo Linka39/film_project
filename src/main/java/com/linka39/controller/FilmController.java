bc9bfa5c6bd548c5b16ff2f110d73d112bac34ae
linka39 <1203440758@qq.com>
2020-05-25 17:16:19 +0800

    v1.020 1、电影列表展示模块开发 2、前端实现分页展示电影

--- src/main/java/com/linka39/controller/FilmController.java
+++ src/main/java/com/linka39/controller/FilmController.java
@@ -3,8 +3,10 @@ package com.linka39.controller;
 import com.linka39.entity.Film;
 import com.linka39.entity.Link;
 import com.linka39.service.FilmService;
+import com.linka39.util.PageUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -48,6 +50,27 @@ public class FilmController {
             mav.addObject("total",filmList.size());
             mav.setViewName("index");   //thymeleaf模板引擎默认返回的是.html文件
         }
+        return mav;
+    }
+
+    /**
+     * 分页查询电影信息
+     * @param page
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/list/{id}")
+    //@PathVariable获取路径中的变量
+    public ModelAndView list(@PathVariable(value = "id",required = false)Integer page) throws Exception{
+        ModelAndView mav = new ModelAndView();
+        List<Film> filmList = filmService.list(null,page,20);
+        Long total = filmService.getCount(null);
+        mav.addObject("title","电影列表");
+        mav.addObject("mainPage","film/list");
+        mav.addObject("mainPageKey","#f");
+        mav.addObject("filmList",filmList);
+        mav.addObject("pageCode", PageUtil.genPagination("/film/list",total,page,20));
+        mav.setViewName("index");
         return mav;
     }
 
